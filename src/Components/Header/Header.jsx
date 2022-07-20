c60c2e213c04d7bf23c046b5997410c38ea38104
Premkumar Nagarajan <nagarajan.premkumar@asda.uk>
2022-07-20 18:36:05 +0530

    Adding Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import styles from "./Header.module.css";
+
+class Header extends Component {
+  render() {
+   
+
+    return (
+      <header className={styles.Header}>
+        <h1>ASDA</h1>
+       </header>
+    );
+  }
+}
+
+
+export default Header;
