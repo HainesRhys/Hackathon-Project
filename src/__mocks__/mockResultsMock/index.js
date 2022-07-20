c78db19c496618553a04c496140bb8537f115218
Rhys Haines <rhys.haines@asda.uk>
2022-07-20 15:34:04 +0100

    mockfile added

---
+++ src/__mocks__/mockResultsMock/index.js
@@ -0,0 +1,30 @@
+export const Results = [
+  {
+    "Name": "ONE POT CHICKEN & TOMATO RICE",
+    "PricePerServing": "£0.84",
+    "CookTime": "35 Mins",
+    "Servings": 4,
+    "Img": "../../Assets/Images/Chicken&RicePot.jpeg"
+  },
+  {
+    "Name": "ONE POT CHICKEN & TOMATO RICE",
+    "PricePerServing": "£0.84",
+    "CookTime": "35 Mins",
+    "Servings": 4,
+    "Img": "../../Assets/Images/Chicken&RicePot.jpeg"
+  },
+  {
+    "Name": "ONE POT CHICKEN & TOMATO RICE",
+    "PricePerServing": "£0.84",
+    "CookTime": "35 Mins",
+    "Servings": 4,
+    "Img": "../../Assets/Images/Chicken&RicePot.jpeg"
+  },
+  {
+    "Name": "ONE POT CHICKEN & TOMATO RICE",
+    "PricePerServing": "£0.84",
+    "CookTime": "35 Mins",
+    "Servings": 4,
+    "Img": "../../Assets/Images/Chicken&RicePot.jpeg"
+  }
+]
