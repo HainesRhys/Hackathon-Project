cd00c8a591d0bb86a838ad5746af0aa2d192c371
Rachana Hegde <vn52quw@uk.wal-mart.com>
2022-07-20 18:42:37 +0100

    test update

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -12,6 +12,14 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Logo from '../Assets/Images/asda_logo.png';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
 
 function Copyright(props) {
   return (
@@ -26,6 +34,46 @@ function Copyright(props) {
   );
 }
 
+//carousel
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'Cherry Artic Roll',
+    imgPath:'https://ui.assets-asda.com/dm/asdagroceries/b4621b7e-d106-11ec-bb7e-25f12b6ec984_Square?scl=1',
+  },
+  {
+    label: 'Parma Violet Seltzer',
+    imgPath:
+      'https://ui.assets-asda.com/dm/asdagroceries/508f6f2f-d10d-11ec-bb7e-f13743cd0482_Square?scl=1',
+  },
+  {
+    label: ' Kadeena Cox – Para-Athletic Champion',
+    imgPath:
+      'https://ui3.assets-asda.com/dm/20210906-brown-stew-chicken__square_GL?scl=2',
+  },
+  {
+    label: 'Chopped Salad',
+    imgPath:
+      'https://ui.assets-asda.com/dm/asdagroceries/475e88df-fe96-11ec-9911-f347fd5c83c1_Square?scl=1',
+  },
+  {
+    label: 'Gin and Basil Smash',
+    imgPath:
+      'https://ui.assets-asda.com/dm/asdagroceries/0badb9d9-d10c-11ec-bb7e-511ed605a7ea_Square?scl=1',
+  },
+  {
+    label: 'One Pot Chicken & Tomato Rice',
+    imgPath:
+      'https://ui.assets-asda.com/dm/asdagroceries/17dc3cb5-fe9e-11ec-9911-3db1f59ca543_Square?scl=1',
+  },
+  {
+    label: 'Cocktails to enjoy the season',
+    imgPath:
+      'https://ui3.assets-asda.com/dm/christmas-cocktails__square_GL?scl=1',
+  },
+];
+
 const theme = createTheme();
 
 export default function SignInSide() {
@@ -37,25 +85,106 @@ export default function SignInSide() {
       password: data.get('password'),
     });
   };
+//carousel
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Grid
-          item
+        <Grid  item
           xs={false}
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            
             backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
+            backgroundColor: 'white'
+          }}>
+        <Box sx={{ maxWidth: 900, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 750,
+                  display: 'block',
+                  maxWidth: 900,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+        </Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
@@ -75,13 +204,13 @@ export default function SignInSide() {
             <Box
         component="img"
         sx={{
-          height: 233,
-          width: 350,
+          height: 50,
+          width: 150,
           maxHeight: { xs: 233, md: 167 },
           maxWidth: { xs: 350, md: 250 },
         }}
         alt="The house from the offer."
-        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+        src={Logo}
       />
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
