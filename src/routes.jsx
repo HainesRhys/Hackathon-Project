a8d7f6dcdff58927560dc31a1e5095c583c4665a
Abhijit Prasad <abhijitprasad14@gmail.com>
2022-07-21 13:44:47 +0530

    added route to signin

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import SignInSide from "./Pages/SignIn";
 
 const AppRouter = () => {
   return (
     <Switch>
-      <Route path="/" exact={true} component={(props) => <Home {...props}/>} />
+      <Route path="/" exact={true} component={(props) => <SignInSide {...props}/>} />
+      
       <Route path="/logout" exact={true} component={(props) => <Home {...props} />} />
     </Switch>
   );
